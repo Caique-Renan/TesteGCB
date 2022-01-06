0f8d479ae84a4a5d48c77974d383bcb1bcf3f71a
Caique Renan <caique.renan21@gmail.com>
2022-01-05 21:19:47 -0300

    Adicao do controller salvar

--- GCB/src/main/java/MedicalController/MedicalController.java
+++ GCB/src/main/java/MedicalController/MedicalController.java
@@ -1,11 +1,15 @@
 package MedicalController;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import domain.MedicalEntity;
 import services.MedicalServices;
@@ -18,5 +22,18 @@ public class MedicalController {
 	public ResponseEntity<List<MedicalEntity>> listar() {
 		return ResponseEntity.status(HttpStatus.OK).body(medicalServices.listar());
 	}
-
+	
+	@RequestMapping(method = RequestMethod.POST)
+	public ResponseEntity<Void> salvar(@Validated @RequestBody MedicalEntity addressRequest) {
+		addressRequest = medicalServices.salvar(addressRequest);
+		
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest()
+				.path("/{id}").buildAndExpand(addressRequest.getClass()).toUri();
+		
+		return ResponseEntity.created(uri).build();
+	
+	}
+	
 }
+
+
