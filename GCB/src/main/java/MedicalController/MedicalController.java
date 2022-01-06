c66fe5c7ec11d515d4f3ca95e6b8ec9445ede616
Caique Renan <caique.renan21@gmail.com>
2022-01-05 21:00:56 -0300

    Adicao do controller listar para listagem de todos os medicos

--- GCB/src/main/java/MedicalController/MedicalController.java
+++ GCB/src/main/java/MedicalController/MedicalController.java
@@ -1,11 +1,22 @@
 package MedicalController;
 
+import java.util.List;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import domain.MedicalEntity;
 import services.MedicalServices;
 
 public class MedicalController {
 	
 	private MedicalServices medicalServices;
 	
-	
+	@RequestMapping(method = RequestMethod.GET)
+	public ResponseEntity<List<MedicalEntity>> listar() {
+		return ResponseEntity.status(HttpStatus.OK).body(medicalServices.listar());
+	}
 
 }
