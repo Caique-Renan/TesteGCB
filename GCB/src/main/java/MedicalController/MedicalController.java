bbb8c39c8ed2e481c9b3b2501d5373b5c3125525
Caique Renan <caique.renan21@gmail.com>
2022-01-05 21:25:37 -0300

    Efetuada adicao do controller atualizar e deletar

--- GCB/src/main/java/MedicalController/MedicalController.java
+++ GCB/src/main/java/MedicalController/MedicalController.java
@@ -6,6 +6,7 @@ import java.util.List;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -33,6 +34,18 @@ public class MedicalController {
 		return ResponseEntity.created(uri).build();
 	
 	}
+	
+	@RequestMapping(method = RequestMethod.PUT)
+	public void atualizar(@RequestBody MedicalEntity medicalRequest) {
+		medicalServices.atualizar(medicalRequest);
+	}
+	
+	@RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
+	public void deletar(@PathVariable("id") Long id) throws Exception {
+		medicalServices.deletar(id);
+	}
+	
+	
 	
 }
 
